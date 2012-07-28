7beb5a6e425eeea0f5940310698c5c8d9195aff3
Samuel Cochran <sj26@sj26.com>
2012-07-28 23:46:47 +0800

    Waiting for action controller to load is causing issues

--- lib/rails/subdomain.rb
+++ lib/rails/subdomain.rb
@@ -5,8 +5,5 @@ end
 
 require 'rails/subdomain/version'
 require 'rails/subdomain/cucumber' if defined? Cucumber
-
-ActiveSupport.on_load :action_controller do
-  require 'rails/subdomain/mapper'
-  require 'rails/subdomain/url_for'
-end
+require 'rails/subdomain/mapper'
+require 'rails/subdomain/url_for'
